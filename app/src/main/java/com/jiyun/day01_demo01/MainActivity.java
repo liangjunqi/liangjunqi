c5566c1a582f6fc942073525b1455ed20630cd6a
lenovo <997980169@qq.com>
2017-09-11 15:58:56 +0800

    第二次

--- app/src/main/java/com/jiyun/day01_demo01/MainActivity.java
+++ app/src/main/java/com/jiyun/day01_demo01/MainActivity.java
@@ -1,15 +1,23 @@
 package com.jiyun.day01_demo01;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
 
 
+    }
+
+    private void initView() {
+        tv = (TextView) findViewById(R.id.tv);
     }
 }
